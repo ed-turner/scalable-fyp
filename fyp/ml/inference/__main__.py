eb97797370a30c120cbd59d6ba40e0476ff60984
Edward Turner <edward.turnerr@gmail.com>
2024-11-21 14:07:03 -0500

    feat: adding cluster

--- fyp/ml/inference/__main__.py
+++ fyp/ml/inference/__main__.py
@@ -4,7 +4,11 @@ This module contains the inference functions for the machine learning models.
 import logging
 
 import numpy as np
+
+from distributed import Client, LocalCluster
+
 import dask.dataframe as dd
+
 from sqlalchemy import create_engine
 import mlflow
 
@@ -23,25 +27,27 @@ results = mlflow.search_registered_models(filter_string=filter_string, max_resul
 logger.info("Fetching the model")
 model = mlflow.sklearn.load_model(f"models:/xgb_ranker/{results[0].latest_versions[0]}")
 
-ddf: dd.DataFrame = dd.read_sql_query(
-    define_inference_data_query(),
-    con=settings.DB_URI,
-    index_col="rn",
-    params={
-        "sslmode": "require",
-        "options": f"-csearch_path%3D{settings.DATA_SOURCE_SCHEMA}"
-    }
-)
+with LocalCluster() as cluster:
+    with Client(cluster):
+        ddf: dd.DataFrame = dd.read_sql_query(
+        define_inference_data_query(),
+        con=settings.DB_URI,
+        index_col="rn",
+        params={
+            "sslmode": "require",
+            "options": f"-csearch_path%3D{settings.DATA_SOURCE_SCHEMA}"
+        }
+        )
 
-result_ddf = ddf.groupby("user_id").apply(
-    lambda df: df.assign(rank=np.argsort(model.predict(df)))
-)
+        result_ddf = ddf.groupby("user_id").apply(
+        lambda df: df.assign(rank=np.argsort(model.predict(df)))
+        )
 
-result_ddf.to_sql(
-    result_ddf,
-    "fyp",
-    uri=settings.DB_URI,
-    schema=settings.ML_PREDICTIONS_SCHEMA,
-    if_exists="replace",
-    parallel=True
-)
+        result_ddf.to_sql(
+        result_ddf,
+        "fyp",
+            uri=settings.DB_URI,
+            schema=settings.ML_PREDICTIONS_SCHEMA,
+            if_exists="replace",
+            parallel=True
+        )
